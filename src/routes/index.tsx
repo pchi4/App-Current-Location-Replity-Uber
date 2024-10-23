ed53e8ec304f2d66bdd23748527c935860ea85ce
Douglas Lopes <pchialoopes@gmail.com>
2024-10-23 10:56:40 -0300

    REAFACTOR: Remove screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Map } from "../screens/Map";
-import { Button } from "../screens/Button";
-import { Home } from "../screens/Home";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,13 +10,6 @@ export const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-
-        <Stack.Screen name="Button" component={Button} />
         <Stack.Screen
           name="Map"
           component={Map}
--- src/screens/Map/index.tsx
+++ src/screens/Map/index.tsx
@@ -1,114 +1,49 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import MapView, {
   Marker,
   PROVIDER_GOOGLE,
-  AnimatedRegion,
   MapCallout,
 } from "react-native-maps";
 import {
-  StyleSheet,
   View,
-  Dimensions,
   Text,
   Platform,
   Image,
-  LogBox,
   SafeAreaView,
   StatusBar,
   TouchableOpacity,
   Alert,
 } from "react-native";
-import { requestForegroundPermissionsAsync } from "expo-location";
 import {
   GooglePlaceDetail,
   GooglePlacesAutocomplete,
 } from "react-native-google-places-autocomplete";
-import MapViewDirections, { Callout } from "react-native-maps-directions";
-const { width, height } = Dimensions.get("window");
+import MapViewDirections from "react-native-maps-directions";
 import { Error } from "../../components/Erro";
-import { useGetCurrentLocation } from "../../hooks";
 import { styles } from "./style";
 import { Info } from "../../components/Info";
-import { useNavigation, useFocusEffect } from "@react-navigation/native";
-
-interface IObject {
-  text: string;
-  value: number | null;
-}
-interface IDeailsCoord {
-  distance: IObject;
-  durantion: IObject;
-  end_address: string;
-  start_address: string;
-}
+import { useFocusEffect } from "@react-navigation/native";
+import { useRequestPermission } from "./hooks/useRequestPermission";
 
 export const Map = ({ navigation }) => {
-  const aspect = width / height;
-  const latitude_delta = 0.0922;
-  const longitude_delta = latitude_delta * aspect;
   const mapRef = useRef();
   const markerRef = useRef();
+  const apiKey = process.env.EXPO_PUBLIC_API_KEY;
 
-  const [isGranted, setIsGranted] = useState(null);
-  const [location, setLocation] = useState({
-    currentLocation: { latitude: -12.9166572, longitude: -38.458494 },
-    destinationCords: { latitude: 0, longitude: 0 },
-    isLoading: true,
-    coordinate: new AnimatedRegion({
-      latitude: -12.9166572,
-      longitude: -38.458494,
-      latitudeDelta: latitude_delta,
-      longitudeDelta: longitude_delta,
-    }),
-    time: 0,
-    distance: 0,
-    heading: 0,
-  });
-
-  const [detailsCoord, setDetailsCoord] = useState<IDeailsCoord>({
-    distance: { text: "", value: null },
-    duration: { text: "", value: null },
-    end_address: "",
-    start_address: "",
-  });
-
-  const [cards] = useState([
-    { title: "Casa", icon: require("../../assets/house.png") },
-    { title: "Trabalho", icon: require("../../assets/work.png") },
-    { title: "Favoritos", icon: require("../../assets/house.png") },
-  ]);
-
-  const { getCurrentLocation } = useGetCurrentLocation();
-
-  const updateState = (data) => setLocation((value) => ({ ...value, ...data }));
-  const updateStateDetails = (data) =>
-    setDetailsCoord((value) => ({ ...value, ...data }));
+  const {
+    requestPermision,
+    updateState,
+    updateStateDetails,
+    isGranted,
+    location,
+    latitude_delta,
+    width,
+    height,
+    longitude_delta,
+    detailsCoord,
+  } = useRequestPermission();
 
   const { coordinates, currentLocation, destinationCords } = location;
-
-  async function requestPermision() {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-
-      setIsGranted(granted);
-      if (granted) {
-        const { latitude, longitude, heading } = await getCurrentLocation();
-
-        updateState({
-          heading: heading,
-          currentLocation: { latitude, longitude },
-          coordinates: new AnimatedRegion({
-            latitude,
-            longitude,
-            latitudeDelta: latitude_delta,
-            longitudeDelta: longitude_delta,
-          }),
-        });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   useFocusEffect(
     React.useCallback(() => {
@@ -249,7 +184,7 @@ export const Map = ({ navigation }) => {
                 origin={currentLocation}
                 destination={destinationCords}
                 optimizeWaypoints={true}
-                apikey="AIzaSyDvNypCJVAfgPJ1nmrqZvz25wSbW3JOjUc"
+                apikey={apiKey}
                 strokeColor="purple"
                 strokeWidth={4}
                 language="pt-br"
@@ -270,24 +205,10 @@ export const Map = ({ navigation }) => {
               flexDirection: "row",
               justifyContent: "space-between",
               alignItems: "center",
+              paddingTop: 16,
             }}
           >
-            <Text style={styles.titel}>Selecione um local</Text>
-            <View
-              style={{
-                backgroundColor: "#89C2D9",
-                padding: 12,
-                borderRadius: 20,
-              }}
-            >
-              <TouchableOpacity onPress={() => navigation.popToTop()}>
-                <Image
-                  width={24}
-                  alt="a"
-                  source={require("../../assets/arrow_back.png")}
-                />
-              </TouchableOpacity>
-            </View>
+            <Text style={styles.titel}>Selecione um endereço</Text>
           </View>
           <View
             style={{
@@ -304,24 +225,12 @@ export const Map = ({ navigation }) => {
                 onPressAddress(details);
               }}
               query={{
-                key: "AIzaSyDvNypCJVAfgPJ1nmrqZvz25wSbW3JOjUc",
+                key: apiKey,
                 language: "pt-br",
                 components: "country:br",
               }}
               debounce={300}
             />
-          </View>
-          <View style={styles.containerCards}>
-            {cards.map((card, idx) => (
-              <View key={idx} style={styles.card}>
-                <Image
-                  source={card.icon}
-                  width={32}
-                  style={{ marginRight: 6 }}
-                />
-                <Text style={{ color: "#2A6F97" }}>{card.title}</Text>
-              </View>
-            ))}
           </View>
         </View>
         <TouchableOpacity onPress={centerlizeCurrentPosition}>
